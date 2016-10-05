60a3d873cfa8c932025525bbd99b3cb1586b5d37
muschellij2 <muschellij2@gmail.com>
2016-10-04 23:48:44 -0400

    adding files to repo

---
+++ R/get_flair_filenames.R
@@ -0,0 +1,15 @@
+#' @title Get FLAIR Image Filenames
+#'
+#' @description Return the filenames for the FLAIR images
+#' @param ... arguments to pass to \code{\link{get_image_filenames}},
+#' \code{modalities = "FLAIR"} so it cannot be specified
+#' @return Vector fo filenames
+#' 
+#' @examples
+#' get_flair_filenames()
+#' @export
+#' @importFrom kirby21.base get_image_filenames
+get_flair_filenames = function(...) {
+  x = kirby21.base::get_image_filenames(modalities = "FLAIR", ...)
+  return(x)
+}
