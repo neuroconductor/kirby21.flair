7e6480c953de360671fd1568706aff8d0bf8d5d3
muschellij2 <muschellij2@gmail.com>
2017-03-01 15:20:05 -0500

    adding readme

--- R/download_flair_data.R
+++ R/download_flair_data.R
@@ -5,10 +5,8 @@
 #' \code{modalities = "FLAIR"} so it cannot be specified
 #' @return Indicator if the data is downloaded.
 #' 
-#' @examples
-#' download_flair_data()
 #' @export
-#' @importFrom kirby21.base download_kirby21_data
+#' @importFrom kirby21.base download_kirby21_data all_modalities
 download_flair_data =  function(...) {  
   x = kirby21.base::download_kirby21_data(modality = "FLAIR", ...)
   return(x)
