c2e51515c7184b29a4d3f0c1ac502abe3235625a
muschellij2 <muschellij2@gmail.com>
2018-02-01 11:06:55 -0500

    updated description with doi

--- R/get_flair_filenames.R
+++ R/get_flair_filenames.R
@@ -3,7 +3,7 @@
 #' @description Return the filenames for the FLAIR images
 #' @param ... arguments to pass to \code{\link{get_image_filenames}},
 #' \code{modalities = "FLAIR"} so it cannot be specified
-#' @return Vector fo filenames
+#' @return Vector of filenames
 #' 
 #' @examples
 #' get_flair_filenames()
